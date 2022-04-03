9de5f5301eb49723402133e1447695ef5d34dca9
NortonPhan <82856263+NortonPhan@users.noreply.github.com>
2022-04-02 18:31:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//https://teachablemachine.withgoogle.com/models/
+//https://www.youtube.com/watch?v=iuy-oOJCOoM dog
+//https://www.youtube.com/watch?v=CY7t8ow2gOM&t=37s cat
+//https://www.youtube.com/watch?v=uFcZhH_wFbs lion
+//https://www.youtube.com/watch?v=dlAgyzTQ5m8 cow
+
+
+//
